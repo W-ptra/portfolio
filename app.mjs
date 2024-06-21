60a2185cc0e5e08b8abb9ee951dc3b533a09fac6
W-ptr <wisnu.putra001@binus.ac.id>
2024-06-22 00:41:56 +0700

    feat: completing project page,add resume and notfound page

--- app.mjs
+++ app.mjs
@@ -38,7 +38,7 @@ app.get("/resume", (req, res) => {
 });
 
 app.use((req, res, next) => {
-    res.status(404).send("<h1>404 Not Found</h1>");
+    res.status(404).sendFile(path.join(__dirname, "./web/notfound404.html"));
 });
 
 app.listen(port, url, () => {
