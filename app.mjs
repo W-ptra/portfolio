081e09a376a05e65c298f548ae5231a0bf59a9ea
W-ptr <wisnu.putra001@binus.ac.id>
2024-06-21 18:45:50 +0700

    reshape

--- app.mjs
+++ app.mjs
@@ -1,22 +1,46 @@
-import {logger} from "./logger.mjs";
+import { logger } from "./logger.mjs";
 import express from "express";
 import cors from "cors";
+import path from "path"; // Import the path module
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 const app = express();
 const url = "0.0.0.0";
-const port = 8080;
+const port = 8000;
+
+// These two lines are necessary to get the __dirname equivalent in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(cors({ origin: '*' }));
 app.use(express.static("web"));
-app.use("/",logger);
+app.use("/", logger);
 
-app.get("/",(req,res)=>{
-    res.status(200).sendFile(path.join(__dirname,"./web/index.html"));
+app.get("/", (req, res) => {
+    res.status(301).redirect("/home");
 });
 
-app.use((req,res,next)=>{
-    res.status(404).send("<h1>404 Not Found</h1>");
-})
+app.get("/home", (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, "./web/home.html"));
+});
+app.get("/about", (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, "./web/about.html"));
+});
+app.get("/skills", (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, "./web/skills.html"));
+});
+app.get("/project", (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, "./web/project.html"));
+});
+app.get("/resume", (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, "./web/resume.html"));
+});
 
-app.listen(port,url,()=>{
+app.use((req, res, next) => {
+    res.status(404).send("<h1>404 Not Found</h1>");
+});
+
+app.listen(port, url, () => {
     console.log(`Listening to ${url}:${port}`);
 });
---
+++ dockerfile
@@ -0,0 +1,19 @@
+# Use the official Node.js 16 image
+FROM node:latest
+
+# Set the working directory in the container
+WORKDIR /usr/src/app
+
+# Copy package.json and package-lock.json to the working directory
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy the rest of the application code to the working directory
+COPY . .
+
+# Expose port 3000
+
+# Set the startup command to run the application
+CMD ["node", "app.mjs"]
